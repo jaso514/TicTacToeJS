d4dd5ff2d8484eafc575d6bc2fefb2f1958340c5
jaso514 <jaso514@users.noreply.github.com>
2017-06-26 12:02:45 -0300

    Commenting index.php

--- index.php
+++ index.php
@@ -6,14 +6,17 @@ require BASEPATH . '/core/Main.php';
 require BASEPATH . '/core/config.php';
 $main = new Main($__config);
 
+/**
+ * Search the Lib or Controller Class to load when is called
+ */
 function __autoload($className){
-    if (file_exists(BASEPATH .DS. 'Libs' .DS. $className . '.php')) {
-    	// find some library
-        require_once(BASEPATH .DS. 'Libs' .DS. $className . '.php');
-    } else if (file_exists(BASEPATH .DS. 'App'.DS.'Controllers' .DS. $className . '.php')) {
-    	// find and controller class, form anotther controller or url
-      require_once(BASEPATH .DS. 'App'.DS.'Controllers' .DS. $className . '.php');
-    } else {
-        /* Error Generation Code Here */
-    }
+  if (file_exists(BASEPATH .DS. 'Libs' .DS. $className . '.php')) {
+    // find some library
+    require_once(BASEPATH .DS. 'Libs' .DS. $className . '.php');
+  } else if (file_exists(BASEPATH .DS. 'App'.DS.'Controllers' .DS. $className . '.php')) {
+    // find and controller class, form anotther controller or url
+    require_once(BASEPATH .DS. 'App'.DS.'Controllers' .DS. $className . '.php');
+  } else {
+    /* Error Generation Code Here */
+  }
 }
